49742a13bbae72b8d041e18c0fd39a17ad040524
ryuzaki3248 <gekidannax7@gmail.com>
2023-12-18 21:51:09 +0900

    cssファイルを反映させるためにpublicフォルダに移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
+# Rails.application.routes.draw do
+#   root to: 'responsives#index'  
+#   resources :responsives, only: :index 
+#   get '/css/:file_name', to: 'responsives#css'
+# end
+
 Rails.application.routes.draw do
-  root to: 'responsives#index'  
-  resources :responsives, only: :index 
+  root to: 'responsives#index'
+  resources :responsives, only: [:index]
+
+  # CSSファイルへのルーティング
+  get '/css/:file_name', to: 'responsives#css'
 end
