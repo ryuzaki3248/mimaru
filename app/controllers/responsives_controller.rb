d6fd337e6022870e931741d27fc854ccaf81b1cb
ryuzaki3248 <gekidannax7@gmail.com>
2024-01-14 09:51:44 +0900

    コントローラーコード一部削除

--- app/controllers/responsives_controller.rb
+++ app/controllers/responsives_controller.rb
@@ -3,8 +3,4 @@ class ResponsivesController < ApplicationController
     @responsives = Responsive.all
   end
 
-  def css
-    file_name = params[:file_name]
-    send_file("#{Rails.root}/public/css/#{file_name}.css", disposition: 'inline')
-  end
 end
